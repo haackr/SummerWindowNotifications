caac98f57e9fe2c391ba8685c5c2c7a48fb23076
Ryan Haack <geekman3232@gmail.com>
2018-05-10 12:12:38 -0600

    Write initial app functionality

--- smartapps/haackr/summer-window-notifications.src/summer-window-notifications.groovy
+++ smartapps/haackr/summer-window-notifications.src/summer-window-notifications.groovy
@@ -3,6 +3,8 @@
  *
  *  Copyright 2018 Ryan Haack
  *
+ *  Powered by WeatherUnderground
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
@@ -23,10 +25,9 @@ definition(
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
     iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
-
 preferences {
-	section("Title") {
-		// TODO: put inputs here
+	section("Indoor Sensor") {
+        input "tempSensorIn", "capability.temperatureMeasurement", required:true
 	}
 }
 
@@ -44,7 +45,45 @@ def updated() {
 }
 
 def initialize() {
-	// TODO: subscribe to attributes, devices, locations, etc.
+    compareTemps()
+}
+
+def getOutsideTemperature() {
+	log.debug "${location.latitude},${location.longitude}"
+	def params = [
+    	uri: "http://api.wunderground.com",
+        path: "/api/3882ac765dbb5837/conditions/q/${location.latitude},${location.longitude}.json"
+    ]
+    try{
+    	httpGet(params) {resp ->
+        	//log.debug "${resp.data}"
+            log.debug "${resp.data.current_observation.temp_f}"
+            return resp.data.current_observation.temp_f
+        }
+    } catch (e) {
+    	log.error "Something went wrong contacting weather undergound: ${e}"
+    }
+}
+
+def compareTemps() {
+	def outsideTemp = getOutsideTemperature()
+    def insideTemp = tempSensorIn.currentValue("temperature")
+    log.debug "inside: ${insideTemp}, outside: ${outsideTemp}"
+    log.debug "last notification: ${state.lastNotification}"
+    if(outsideTemp != null && insideTemp != null) {
+        if (outsideTemp > insideTemp && state.lastNotification != "close"){
+            state.lastNotification = "close"
+            log.debug "Close the windows - last notification: ${state.lastNotification}"
+            sendPush("Close the windows, it's hot outside!")
+        }else if (outsideTemp < insideTemp && state.lastNotification != "open"){
+            state.lastNotification = "open"
+            log.debug "Open the windows - last notification: ${state.lastNotification}"
+            sendPush("Open the windows, it's cooling off!")
+        }else {
+            log.debug "Do nothing"
+        }
+    }
+    runIn(60*10,compareTemps)
 }
 
 // TODO: implement event handlers
